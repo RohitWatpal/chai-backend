dbcf2555d321f4f0c4d279f5fa6f26823f2424b3
RohitWatpal <watpalrohit@gmail.com>
2026-08-27 15:53:14 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+    (req, res , next) => {
+        Promise.resolve().catch((err) => next (err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+// const asyncHandler = (fun) => async (req, res, next) => {
+//     try{
+
+//     }catch(error)  {
+//         res.status(err.code || 500).json({
+
+//             success:false,
+//             message:err.message
+//         })
+//     }
+    
+// }
